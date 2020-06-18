549067adcfe22ce60137b10c5f47ca6686902711
phil cowan <philipjcowan1@gmail.com>
2020-06-18 16:50:41 -0700

    adds react stacknavigation

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text>SearchScreen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default SearchScreen;
