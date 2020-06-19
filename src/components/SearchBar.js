6f208c0d61601da05dcab44dc82109a60aa543bd
phil cowan <philipjcowan1@gmail.com>
2020-06-18 17:45:09 -0700

    styles searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,20 +4,32 @@ import { TextInput } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 const SearchBar = () => {
     return (
-        <View style={styles.background}>
-            <Feather name="search" size={30}/>
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" style={styles.iconStyle} />
             <TextInput
-            placeholder="searchbar" />
+            style={styles.inputStyle}
+            placeholder="Search" />
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
         backgroundColor: '#F0EEEE',
         height: 50,
+        marginTop: 10,
         borderRadius: 5,
-        marginHorizontal:25
+        marginHorizontal:15,
+        flexDirection: 'row',
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18,
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 10
     }
 });
 
