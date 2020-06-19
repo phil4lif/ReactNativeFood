3aae9ce0d5077a4ab537d4dfa3e44096bf6cae83
phil cowan <philipjcowan1@gmail.com>
2020-06-18 21:47:56 -0700

    seperating results into three lists based on price

---
+++ src/components/ResultsList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+const ResultsList = ({ title, results }) => {
+    return (
+        <View>
+            <Text style={styles.titleStyle}>{title}</Text>
+            <Text>Results: {results.length}</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    titleStyle: {
+        fontSize: 18,
+        fontWeight:'bold'
+    }
+});
+
+export default ResultsList;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp'
 import useResults from '../hooks/useResults'
+import ResultsList from '../components/ResultsList';
+
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
-    
+
+    const filterResultsByPrice = (price) => {
+        //price === $ || $$ || $$$
+        return results.filter(result => {
+            return result.price === price
+        });
+    };
+
     return (
         <View>
             <SearchBar
@@ -16,6 +25,9 @@ const SearchScreen = () => {
                 onTermSubmit={() => searchApi(term)} />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
+            <ResultsList results={filterResultsByPrice('$')} title="$"/>
+            <ResultsList results={filterResultsByPrice('$$')} title="$$"/>
+            <ResultsList results={filterResultsByPrice('$$$')} title="$$$"/>
         </View>
     )
 };
