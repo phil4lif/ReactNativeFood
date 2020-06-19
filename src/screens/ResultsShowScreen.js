6d626d04e6dbb7a8f39c7a4c199995aaaf6260d4
phil cowan <philipjcowan1@gmail.com>
2020-06-19 08:26:59 -0700

    adds async api call for single result with use effect

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
-
+import yelp from '../api/yelp'
 const ResultsShowScreen = ({ navigation }) => {
+    const [result, setResult] = useState(null);
     const id = navigation.getParam('id')
-    console.log(id)
+    console.log(result);
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        response.data;
+        setResult(response.data);
+    };
+    useEffect(() => {
+        getResult(id);
+    }, []);
     return (
         <>
             <Text>results show screen</Text>
