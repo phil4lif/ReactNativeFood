0848d3eaeb3a5b9e89492e1318139bf53c742d32
phil cowan <philipjcowan1@gmail.com>
2020-06-18 20:49:27 -0700

    adds use effect hook to load a default search on first render

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp'
@@ -8,26 +8,32 @@ const SearchScreen = () => {
     const [results, setResults] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const searchApi = async () => {
+    const searchApi = async (searchTerm) => {
+        console.log('hi there');
         try {
             const response = await yelp.get('/search', {
                 params: {
                     limit: 50,
-                    term,
+                    term: searchTerm,
                     location: 'phoenix'
-                }
+                }   
             });
             setResults(response.data.businesses);
         } catch (err) {
             setErrorMessage('soething went wrong')
         }
-    }
+    };
+    //call search api when component is first rendered
+    useEffect(() => {
+        searchApi('pasta');
+    }, []);
+
     return (
         <View>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
-                onTermSubmit={searchApi} />
+                onTermSubmit={() => searchApi(term)} />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
         </View>
